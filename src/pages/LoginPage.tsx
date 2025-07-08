ce7b2618bbb57a504f20830c3284dbd29b8d9276
idiot <qpalqpzm999@gmail.com>
2025-07-08 20:48:26 +0700

    0.9.1
    - Исправлен переход на страницу /login, когда пользователь уже был авторизован. Теперь нужно быть неавторизованным (в прошлый коммит это изменение не вошло)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react'; // 1. Импортируем useEffect
 import { useState } from 'react';
 import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom'; // Импортируем useNavigate
 import { loginUser, setInitialPassword } from '../api/authService';
 
 export const LoginPage = () => {
@@ -7,7 +9,16 @@ export const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState(''); 
   const [isFirstLogin, setIsFirstLogin] = useState(false);
-  const { login: authLogin } = useAuth();
+  const { isAuthenticated, login: authLogin } = useAuth(); // 2. Получаем статус авторизации
+  const navigate = useNavigate();
+
+  // 3. Добавляем эффект, который сработает при загрузке компонента
+  useEffect(() => {
+    // Если пользователь уже авторизован, перенаправляем его
+    if (isAuthenticated) {
+      navigate('/app', { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   const handleLogin = async () => {
     setErrorMessage('');
